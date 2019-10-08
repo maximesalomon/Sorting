fd598f4ece18d73d7b0e446d2e2aec2daf50112f
Maxime Salomon <maxime@croissant.io>
2019-10-08 16:41:22 +0200

    merge

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,9 +1,23 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    
+    x = 0
+    y = 0
+    for i in range(0, elements):
+        if x >= len(arrA):
+            merged_arr[i] = arrB[y]
+            y += 1
+        elif y >= len(arrB):
+            merged_arr[i] = arrA[x]
+            x += 1
+        elif arrA[x] < arrB[y]:
+            merged_arr[i] = arrA[x]
+            x += 1
+        else:
+            merged_arr[i] = arrB[y]
+            y += 1
     return merged_arr
 
 
